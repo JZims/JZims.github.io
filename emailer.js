85f89c64da8a241e8876d256228041df6ad09d20
JZims <zimmerman.j@hotmail.com>
2021-08-26 00:48:01 -0400

    resume updated

---
+++ emailer.js
@@ -0,0 +1,16 @@
+// import dotenv from "dotenv"
+// import cors from "cors"
+
+require('dotenv').config()
+
+const emailForm = document.querySelector("#contact-form")
+
+
+emailForm.addEventListener("submit", (e) => {
+e.preventDefault()
+    console.log(e)
+
+
+
+})
+
